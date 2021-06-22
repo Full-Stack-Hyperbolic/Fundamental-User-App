b28705fcd3f0e78e9ac24662b527adf9e745d38c
Karsenh <KarsenHansen@gmail.com>
2021-06-22 02:46:24 -0700

    setup basics for express app

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ extended: true}))
+app.use(cors());
