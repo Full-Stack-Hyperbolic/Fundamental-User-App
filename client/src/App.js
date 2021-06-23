867343b3e193ae3cb8de063e3f4612eab873d5fb
Karsenh <KarsenHansen@gmail.com>
2021-06-22 23:44:38 -0700

    created the Form, Posts, and Post (child) component to bring into App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from './images/memories.png';
 
 const App = () => {
@@ -11,6 +14,25 @@ const App = () => {
         </Typography>
         <img src={memories} alt='memories' height='500' />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify='space-between'
+            alignItems='stretch'
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              {/* Bring in custom POSTS component */}
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* Bring in custom FORM component */}
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
