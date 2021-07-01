962d8ed5f559921fadca40e88cce344ff6668416
Karsenh <KarsenHansen@gmail.com>
2021-07-01 02:47:03 -0700

    brought in cors to fix browser console error

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-import postRoutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-// Use express middleware to bring in our routes route using 'posts' prefix
-// Reachable on `localhost:5000/posts`
-app.use('/posts', postRoutes);
-
+// Apply our middleware before brining in our routes
 app.use(express.json({ limit: '30mb', extended: true }));
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://admin-Karsen:admin-Karsen1212@cluster0.2rxhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+// Reachable on `localhost:5000/posts`
+// Use express middleware to bring in our routes route using 'posts' prefix
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL =
+  'mongodb+srv://admin-Karsen:admin-Karsen1212@cluster0.2rxhx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`)))
-.catch((error) => console.log(error.message))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
+  )
+  .catch(error => console.log(error.message));
 
 // ensures no warnings printed to console
 mongoose.set('useFindAndModify', false);
