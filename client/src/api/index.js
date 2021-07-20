232c5e97fe3e0f426463ce721ce341dfad8cda5e
Karsenh <KarsenHansen@gmail.com>
2021-07-20 14:30:10 -0700

    Can now delete posts

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,5 +6,10 @@ const url = 'http://localhost:5000/posts';
 // axios get request to our API for returning posts
 export const fetchPosts = () => axios.get(url);
 export const createPost = newPost => axios.post(url, newPost);
-export const updatePost = (id, updatedPost) =>
+export const updatePost = (id, updatedPost) => {
   axios.patch(`${url}/${id}`, updatedPost);
+};
+
+export const deletePost = id => {
+  axios.delete(`${url}/${id}`);
+};
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -33,6 +33,7 @@ export const updatePost = async (req, res) => {
   const { id: _id } = req.params;
   const post = req.body;
 
+  // Check that the id is VALID
   if (!mongoose.Types.ObjectId.isValid(_id)) {
     return res.status(404).send('No post with that id');
   }
@@ -44,4 +45,17 @@ export const updatePost = async (req, res) => {
   );
 
   res.json(updatedPost);
+};
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+
+  // Check that the id is VALID
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).send('No post with that id');
+  }
+
+  await PostMessage.findByIdAndRemove(id);
+
+  res.json({ message: 'Post successfully deleted!' });
 };
